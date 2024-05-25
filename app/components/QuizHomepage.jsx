bb2e5bb60c5ecd209b4235336b0f158e17303b70
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-05-25 13:30:47 +0100

    homepage finished

--- app/components/QuizHomepage.jsx
+++ app/components/QuizHomepage.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HistoryHomepage = () => {
+const QuizHomepage = () => {
  
   return (
     <div className="relative mt-20 z-20">
       <div id="welcome-container" className="p-6 bg-black bg-opacity-80 mx-auto text-center text-white md:max-w-[100%]">
         <div className="text-xl flex items-center justify-center inset-x-0 mb-4">
-        <span className='underline hover:text-green-600'><Link href="/history" passHref>History of Dry Stone Walling</Link></span>
+        <span className='underline hover:text-green-600'><Link href="/quiz" passHref>Test your knowledge</Link></span>
         </div>
         <div className="text-lg">
           <p>
-          Delve into the past and uncover the ancient art of dry stone walling...
+          How will you do in the dry stone walling quiz? 
           </p>
           <div className='flex w-full items-center justify-center'>
           <img
-          src='../images/orkney-stones.jpg'
+          src='../images/tree-dry-stone-wall.jpg'
           className="m-4 max-h-60 max-w-60 lg:max-h-80 lg:max-w-80"
-          alt='Orkney Stones of Stenness'
+          alt='Dry stone wall leading to tree'
           />
           </div>
-          <p className='underline hover:text-green-600'><Link href="/history" passHref>Discover more about this timeless craft</Link>
+          <p className='underline hover:text-green-600'><Link href="/quiz" passHref>Lets go!</Link>
           </p>
           
         </div>
@@ -29,4 +29,4 @@ const HistoryHomepage = () => {
   );
 };
 
-export default HistoryHomepage;
+export default QuizHomepage;
