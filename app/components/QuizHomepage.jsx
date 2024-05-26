3b8c6df15e58d30800f2e2e3b98458eb1c660788
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-05-26 03:05:22 +0100

    walling services cleaned up

--- app/components/QuizHomepage.jsx
+++ app/components/QuizHomepage.jsx
@@ -5,7 +5,7 @@ const QuizHomepage = () => {
  
   return (
     <div className="relative mt-20 z-20">
-      <div id="welcome-container" className="p-6 bg-black bg-opacity-80 mx-auto text-center text-white md:max-w-[100%]">
+      <div id="quiz-homepage-container" className="p-6 bg-black bg-opacity-80 mx-auto text-center text-white md:max-w-[100%]">
         <div className="text-xl flex items-center justify-center inset-x-0 mb-4">
         <h3 className='underline hover:text-green-600 font-bold'><Link href="/quiz" passHref>Test your knowledge</Link></h3>
         </div>
