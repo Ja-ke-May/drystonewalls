7df620a02eb34c448a7156143a1aa6243aafc109
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2024-05-30 21:57:53 +0100

    titles all made h1

--- app/homepage/components/QuizHomepage.jsx
+++ app/homepage/components/QuizHomepage.jsx
@@ -7,7 +7,7 @@ const QuizHomepage = () => {
     <div className="relative mt-20 z-20">
       <div id="quiz-homepage-container" className="p-6 bg-black bg-opacity-80 mx-auto text-center text-white md:max-w-[100%]">
         <div className="text-xl flex items-center justify-center inset-x-0 mb-4">
-        <h3 className='underline hover:text-green-600 font-bold'><Link href="/quiz" passHref>Test your Knowledge</Link></h3>
+        <h3 className='underline hover:text-green-600 font-bold'><Link href="/quiz" passHref>Test Your Knowledge</Link></h3>
         </div>
         <div className="text-lg">
           <p>
